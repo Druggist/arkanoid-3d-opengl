c3cca30ab7f2905fd9bc61337554765ead0f40a0
Druggist <m.najdora@gmail.com>
2017-06-04 22:37:17 +0200

    Fixed textures generation

--- renderer.cpp
+++ renderer.cpp
@@ -21,25 +21,20 @@ GLuint Renderer::ReadTexture(const char* filename) {
 	GLuint tex;
 	glActiveTexture(GL_TEXTURE0);
 
-	//Wczytanie do pamięci komputera
-	std::vector<unsigned char> image;   //Alokuj wektor do wczytania obrazka
-	unsigned width, height;   //Zmienne do których wczytamy wymiary obrazka
-	//Wczytaj obrazek
+	std::vector<unsigned char> image;
+	unsigned width, height;
 	unsigned error = lodepng::decode(image, width, height, filename);
 
-	if (error!=0) {
-        printf("Error while reading texture %s. Error code: %d. \n",filename,error);
-	}
+	if (error!=0) printf("Error while reading texture %s. Error code: %d. \n",filename,error);
 
-	//Import do pamięci karty graficznej
-	glGenTextures(1,&tex); //Zainicjuj jeden uchwyt
-	glBindTexture(GL_TEXTURE_2D, tex); //Uaktywnij uchwyt
-	//Wczytaj obrazek do pamięci KG skojarzonej z uchwytem
-	glTexImage2D(GL_TEXTURE_2D, 0, 4, width, height, 0,
-	GL_RGBA, GL_UNSIGNED_BYTE, (unsigned char*) image.data());
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+	glGenTextures(1,&tex); 
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_BGRA, GL_UNSIGNED_BYTE, (unsigned char*) image.data());
+	glGenerateMipmap(GL_TEXTURE_2D);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 
 	return tex;
 }
